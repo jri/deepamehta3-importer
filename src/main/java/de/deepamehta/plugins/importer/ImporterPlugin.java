31516369d00d0e9119d2ca016f5ac2d08701f2f8
Jörg Richter <jri@deepamehta.de>
2011-02-28 04:23:00 +0100

    Adapt to current version of deepamehta3-core
    New core classes CommandParams and CommandResult.
    
    Adapted to new build system:
    The parent project for 3rd party plugins is now deepamehta3-plugin-parent.
    The deepamehta3-core dependency must no longer be declared.
    The bundle plugin configuration no longer requires <extensions>true</extensions>.

--- src/main/java/de/deepamehta/plugins/importer/ImporterPlugin.java
+++ src/main/java/de/deepamehta/plugins/importer/ImporterPlugin.java
@@ -1,35 +1,27 @@
 package de.deepamehta.plugins.importer;
 
 import de.deepamehta.core.model.ClientContext;
-import de.deepamehta.core.model.DataField;
-import de.deepamehta.core.model.Topic;
-import de.deepamehta.core.model.TopicType;
+import de.deepamehta.core.model.CommandParams;
+import de.deepamehta.core.model.CommandResult;
 import de.deepamehta.core.service.Plugin;
 import de.deepamehta.core.util.UploadedFile;
-
-import org.codehaus.jettison.json.JSONArray;
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.logging.Logger;
 
 
 
 public class ImporterPlugin extends Plugin {
 
-    // -------------------------------------------------------------------------------------------------- Static Methods
-
     // ---------------------------------------------------------------------------------------------- Instance Variables
 
     private Logger logger = Logger.getLogger(getClass().getName());
+
+    // -------------------------------------------------------------------------------------------------- Public Methods
 
 
 
@@ -40,19 +32,16 @@ public class ImporterPlugin extends Plugin {
 
 
     @Override
-    public JSONObject executeCommandHook(String command, Map params, ClientContext clientContext) {
+    public CommandResult executeCommandHook(String command, CommandParams params, ClientContext clientContext) {
         if (command.equals("deepamehta3-importer.start")) {
             UploadedFile file = null;
             try {
-                file = (UploadedFile) params.get("file");
+                file = params.getFile("file");
                 logger.info("### Importing " + file);
                 importFile(file);
-                //
-                JSONObject result = new JSONObject();
-                result.put("message", "OK");
-                return result;
-            } catch (Throwable e) {
-                throw new RuntimeException("Error while importing " + file, e);
+                return new CommandResult("message", "OK");
+            } catch (Exception e) {
+                throw new RuntimeException("Importing " + file + " failed", e);
             }
         }
         return null;
@@ -61,7 +50,7 @@ public class ImporterPlugin extends Plugin {
     // ------------------------------------------------------------------------------------------------- Private Methods
 
     private void importFile(UploadedFile file) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file.getFile())));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
         StringBuilder builder = new StringBuilder();
         builder.append(reader.readLine() + "\n");
         builder.append(reader.readLine() + "\n");
